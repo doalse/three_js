6e224ec7fb16f6d07f09caff7c1a90679af4ac7b
Oleksandr <doalsekeeper@gmail.com>
2024-10-21 18:33:07 +0300

    commit2

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,3 @@
-// import commonjs from 'rollup-plugin-commonjs';
-
-// export default {
-//   input: 'node_modules/three/build/three.module.js', // Вхідний файл з ES2014 модулями
-//   output: {
-//     file: 'dist/three.js', // Вихідний файл
-//     format: 'iife', // Формат для браузерів
-//     name: 'THREE', // Глобальна змінна
-//   },
-//   plugins: [
-//     commonjs() // Використовуємо плагін для конвертації модулів
-//   ]
-// };
-
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
